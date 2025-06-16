57eb726ae55dd7d176dd19c2a81665e8bd670088
akggautamasar <Worksbeyondworks@gmail.com>
2025-06-16 13:16:21 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then(() => {
+      console.log("✅ Service Worker registered");
+    })
+    .catch((error) => {
+      console.error("❌ Service Worker registration failed:", error);
+    });
+}
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
